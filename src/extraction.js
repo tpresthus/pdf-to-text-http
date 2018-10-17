d9364243bd3573306d64b0b61c3213c8a2cfa38b
Thomas Presthus <thomas@erus.net>
2018-10-17 17:28:48 +0200

    Utility function for downloading PDFs

--- src/extraction.js
+++ src/extraction.js
@@ -1,4 +1,35 @@
 var extract = require('pdf-text-extract');
+var request = require('request');
+var fs = require('fs');
+
+var downloadFile = function(url, callback) {
+  // TODO: Generate random filename
+  var filePath = "/tmp/foo.pdf";
+  var file = fs.createWriteStream(filePath);
+
+  var sendReq = request.get(url);
+
+  sendReq.on('response', function(response) {
+    if(response.statusCode != 200)
+      return callback(response.statusCode);
+  });
+
+  sendReq.on('error', function(err) {
+    fs.unlink(filePath);
+    return callback(err.message);
+  });
+
+  sendReq.pipe(file);
+
+  file.on('finish', function() {
+    file.close(callback);
+  });
+
+  file.on('error', function() {
+    fs.unlink(filePath);
+    return callback(err.message);
+  });
+};
 
 var handleFile = function(filePath, callback) {
   extract(filePath, { splitPages: false }, callback);
